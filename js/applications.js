3b1b9e48f645151f8cbf43d6529a673213b9b365
NessySweet <va.ogbuehi@gmail.com>
2015-02-16 12:59:20 -0500

    Add check/uncheck feature

--- js/applications.js
+++ js/applications.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-	$('.addButton').click(function() {
-		if ($('.addItem').val().trim().length == 0) {
-			//alert("Put someting here!");
-			$('.error').show();
-		}
 
-		else {
-			//alert('Something is gonna happen');
-			$('ul').append('<li class="item"><div class="checkbox"><p class="checkmark">&#10003;</p></div><p>' + $('.addItem').val() + '</p><p class="delete">x</p>');
-			$('.error').hide();
-		};
-	})
+    //Validates text box, then adds value in text box to list
+
+    $('.addButton').click(function() {
+        if ($('.addItem').val().trim().length == 0) {
+            //alert("Put someting here!");
+            $('.error').show();
+        }
+
+        else {
+            //alert('Something is gonna happen');
+            $('ul').append('<li class="item"><div class="checkbox"><p class="checkmark">&#10003;</p></div><p>' + $('.addItem').val() + '</p><p class="delete">x</p>');
+            $('.error').hide();
+        };
+    })
+
+    $('.checkbox').click(function() {
+        $(this).children("p").toggle();
+
+    })
 })
